1e1e219fdb7ce25d04bf809d242b43c402ca57b1
João Bispo <joaobispo2077@gmail.com>
2022-01-22 17:06:28 -0300

    test: ensure SingUpController return 422 if no name is provided

---
+++ tests/presentation/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from '../../../src/presentation/controllers/signup';
+
+describe('SignUp Controller', () => {
+  it('should be able to return 422 if no name is provided', async () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: 'John Doe',
+        email: 'john.doe@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(422);
+  });
+});
