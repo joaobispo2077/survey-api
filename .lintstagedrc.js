19e8cae5957c2c2be7a8acaa1929cec4f22fd005
João Bispo <joaobispo2077@gmail.com>
2022-01-22 15:56:21 -0300

    chore: run lint into changed files when commit \n\n with husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  '(src|tests)/**/*.ts': ['prettier --write', 'eslint --fix'],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-console.log('Hello World');
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function test(): string {
   return 'test';
