680193ad2c9657bcbb1e3bf91832e332f5d818a8
João Bispo <joaobispo2077@gmail.com>
2022-01-23 11:16:45 -0300

    feat: ensure SignUpController returns 422 if an invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,13 @@
+import { InvalidParamError } from '../errors/InvalidParamError';
 import { MissingParamError } from '../errors/MissingParamError';
 import { unprocessableEntity } from '../helpers/http';
-import { Controller } from '../protocols/controller';
-import { HttpRequest, HttpResponse } from '../protocols/http';
+import { Controller } from '../protocols/Controller';
+import { EmailValidator } from '../protocols/EmailValidator';
+import { HttpRequest, HttpResponse } from '../protocols/Http';
 
 export class SignUpController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = [
       'name',
@@ -16,6 +20,14 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return unprocessableEntity(new MissingParamError(field));
       }
+    }
+
+    const { email } = httpRequest.body;
+
+    const isEmailValid = this.emailValidator.isValid(email);
+
+    if (!isEmailValid) {
+      return unprocessableEntity(new InvalidParamError('email'));
     }
   }
 }
---
+++ src/presentation/errors/InvalidParamError.ts
@@ -0,0 +1,6 @@
+export class InvalidParamError extends Error {
+  constructor(field: string) {
+    super(`Invalid param: ${field}`);
+    this.name = 'InvalidParamError';
+  }
+}
