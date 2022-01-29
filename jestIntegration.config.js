0ef4e9669bb00cc6980934c20abe325d9b359f90
João Bispo <joaobispo2077@gmail.com>
2022-01-29 10:58:44 -0300

    chore: setup mongodb in memory to run integration tests

--- jestIntegration.config.js
+++ jestIntegration.config.js
@@ -1,8 +1,11 @@
 const config = require('./jest.config');
+const { defaults: tsjPreset } = require('ts-jest/presets');
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   ...config,
   displayName: 'integration-tests',
   testMatch: ['<rootDir>/tests/**/*.test.ts'],
+  transform: tsjPreset.transform,
+  preset: '@shelf/jest-mongodb',
 };
