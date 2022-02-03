5ee9dfdeeb1cfd87d1fa115b03e4fd479df71877
João Bispo <joaobispo2077@gmail.com>
2022-02-03 17:14:15 -0300

    refactor: group middleware import in a single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,6 @@
 import express, { Express } from 'express';
 
-import { bodyParser } from '../middlewares/bodyParser';
-import { contentType } from '../middlewares/contentType';
-import { cors } from '../middlewares/cors';
+import { bodyParser, contentType, cors } from '../middlewares';
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser);
