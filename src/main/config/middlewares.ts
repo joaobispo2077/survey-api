1526dafd0815fa65586388f2ea7e598415b09b57
João Bispo <joaobispo2077@gmail.com>
2022-02-03 17:09:46 -0300

    feat: ensure json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
 
 import { bodyParser } from '../middlewares/bodyParser';
+import { contentType } from '../middlewares/contentType';
 import { cors } from '../middlewares/cors';
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser);
   app.use(express.urlencoded({ extended: true }));
   app.use(cors);
+  app.use(contentType);
 };
---
+++ src/main/middlewares/contentType.ts
@@ -0,0 +1,7 @@
+import { RequestHandler } from 'express';
+
+export const contentType: RequestHandler = (request, response, next) => {
+  response.type('json');
+
+  next();
+};
