1fcf8e61904604cc2dea86b5bf237f761348269e
João Bispo <joaobispo2077@gmail.com>
2022-02-05 15:41:22 -0300

    refactor: move sut creation to a helper factory method

--- tests/unit/main/decorators/LoggerControllerDecorator.spec.ts
+++ tests/unit/main/decorators/LoggerControllerDecorator.spec.ts
@@ -4,22 +4,36 @@ import {
   HttpRequest,
   HttpResponse,
 } from '../../../../src/presentation/protocols';
+LoggerControllerDecorator;
+
+interface SutResponsePayload {
+  sut: LoggerControllerDecorator;
+  controllerStub: Controller;
+}
+
+const makeSut = (): SutResponsePayload => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      return {
+        statusCode: 200,
+        body: {
+          name: 'any_name',
+        },
+      };
+    }
+  }
+
+  const controllerStub = new ControllerStub();
+  const sut = new LoggerControllerDecorator(controllerStub);
+  return {
+    sut,
+    controllerStub,
+  };
+};
 
 describe('LoggerControllerDecorator', () => {
   it('should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        return {
-          statusCode: 200,
-          body: {
-            name: 'any_name',
-          },
-        };
-      }
-    }
-
-    const controllerStub = new ControllerStub();
-    const sut = new LoggerControllerDecorator(controllerStub);
+    const { sut, controllerStub } = makeSut();
     const controllerStubHandleSpy = jest.spyOn(controllerStub, 'handle');
 
     const httoRequest = {
