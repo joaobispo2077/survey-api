a16643c2cb0456e1b6f93d34404217332f16f339
João Bispo <joaobispo2077@gmail.com>
2022-02-05 16:15:19 -0300

    feat: ensure LoggerControllerDecorator make log when controller throws

--- src/main/decorators/LoggerControllerDecorator.ts
+++ src/main/decorators/LoggerControllerDecorator.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 
+import { LoggerErrorRepository } from '../../data/protocols/LoggerErrorRepository';
 import {
   Controller,
   HttpRequest,
@@ -7,10 +8,17 @@ import {
 } from '../../presentation/protocols';
 
 export class LoggerControllerDecorator implements Controller {
-  constructor(private readonly controller: Controller) {}
+  constructor(
+    private readonly controller: Controller,
+    private readonly loggerErrorRepository: LoggerErrorRepository,
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest);
+
+    if (httpResponse.statusCode === 500) {
+      await this.loggerErrorRepository.log(httpResponse.body.stack);
+    }
 
     console.log(httpResponse.body);
     return httpResponse;
