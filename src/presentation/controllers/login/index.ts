91eab78c7d4b3470b485b6975504cb3365f7dd9e
João Bispo <joaobispo2077@gmail.com>
2022-02-06 17:25:56 -0300

    feat: ensure LoginController pass received values to EmailValidator calls isValid

--- src/presentation/controllers/login/index.ts
+++ src/presentation/controllers/login/index.ts
@@ -1,8 +1,11 @@
-import { MissingParamError } from '../../errors';
+import { InvalidParamError, MissingParamError } from '../../errors';
 import { unprocessableEntity } from '../../helpers/http';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
+import { EmailValidator } from '../signUp/signUpProtocols';
 
 export class LoginController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return unprocessableEntity(new MissingParamError('email'));
@@ -11,5 +14,7 @@ export class LoginController implements Controller {
     if (!httpRequest.body.password) {
       return unprocessableEntity(new MissingParamError('password'));
     }
+
+    this.emailValidator.isValid(httpRequest.body.email);
   }
 }
