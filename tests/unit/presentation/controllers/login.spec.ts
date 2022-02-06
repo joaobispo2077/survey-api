8d2708d89907cf5fe910439acde93259960af633
João Bispo <joaobispo2077@gmail.com>
2022-02-06 17:19:58 -0300

    test: ensure LoginController reeturns 422 if no password is provided

--- tests/unit/presentation/controllers/login.spec.ts
+++ tests/unit/presentation/controllers/login.spec.ts
@@ -2,9 +2,20 @@ import { LoginController } from '../../../../src/presentation/controllers/login'
 import { MissingParamError } from '../../../../src/presentation/errors';
 import { unprocessableEntity } from '../../../../src/presentation/helpers/http';
 
+interface SutResponsePayload {
+  sut: LoginController;
+}
+
+const makeSut = (): SutResponsePayload => {
+  const sut = new LoginController();
+  return {
+    sut,
+  };
+};
+
 describe('Login Controller', () => {
-  it('should return 400  if no email is provided', async () => {
-    const sut = new LoginController();
+  it('should return 422  if no email is provided', async () => {
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
@@ -15,6 +26,21 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(
       unprocessableEntity(new MissingParamError('email')),
+    );
+  });
+
+  it('should return 422  if no password is provided', async () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmai.com',
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(
+      unprocessableEntity(new MissingParamError('password')),
     );
   });
 });
