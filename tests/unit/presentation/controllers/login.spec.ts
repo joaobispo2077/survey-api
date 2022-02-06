c171992ff0f3f981ac02e50cb25826e478b60ad4
João Bispo <joaobispo2077@gmail.com>
2022-02-06 17:26:21 -0300

    test: ensure LoginController pass received values to EmailValidator calls isValid

--- tests/unit/presentation/controllers/login.spec.ts
+++ tests/unit/presentation/controllers/login.spec.ts
@@ -1,15 +1,27 @@
 import { LoginController } from '../../../../src/presentation/controllers/login';
 import { MissingParamError } from '../../../../src/presentation/errors';
 import { unprocessableEntity } from '../../../../src/presentation/helpers/http';
+import { EmailValidator } from '../../../../src/presentation/protocols/EmailValidator';
 
 interface SutResponsePayload {
   sut: LoginController;
+  emailValidatorStub: EmailValidator;
 }
 
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
+
 const makeSut = (): SutResponsePayload => {
-  const sut = new LoginController();
+  const emailValidatorStub = makeEmailValidator();
   return {
-    sut,
+    sut: new LoginController(emailValidatorStub),
+    emailValidatorStub,
   };
 };
 
@@ -42,5 +54,20 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(
       unprocessableEntity(new MissingParamError('password')),
     );
+  });
+
+  it('should call EmailValidator with received email', () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
+
+    const httpRequest = {
+      body: {
+        email: 'valid_email@gmail.com',
+        password: 'valid_password123',
+      },
+    };
+
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email);
   });
 });
